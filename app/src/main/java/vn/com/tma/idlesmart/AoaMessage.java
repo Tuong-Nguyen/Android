c2ef0d7f0032bf4cbf029c2a154154b5c4ac0298
Le Ngoc Thao <lnthao@TMAITs-iMac.local>
2017-03-30 10:59:48 +0700

    Add AoaMessage class for declaring constant for parsing the message

---
+++ app/src/main/java/vn/com/tma/idlesmart/AoaMessage.java
@@ -0,0 +1,11 @@
+package vn.com.tma.idlesmart;
+
+/**
+ * Created by lnthao on 3/30/17.
+ */
+
+
+public class AoaMessage {
+    public static final int HEADER_LENGTH = 2;
+    public static final int MESSAGE_ID_POSITION = 2;
+}
