0890b6317a95524f84b45de08e6e73fe3ec9a26f
Hanh <ntmhanh@tma.com.vn>
2017-04-04 13:42:56 +0700

    Extract to running_fragment

---
+++ app/src/main/java/vn/com/tma/idlesmart/fragments/RunningFragment.java
@@ -0,0 +1,33 @@
+package vn.com.tma.idlesmart.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vn.com.tma.idlesmart.R;
+
+
+public class RunningFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.running_fragment, container, false);
+    }
+
+
+
+}
