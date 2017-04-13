b5f3d820e3454bd73c59dba8848eb737dc1e4049
Hanh <ntmhanh@tma.com.vn>
2017-04-13 14:20:15 +0700

    - Rename StringAndVoltageUtils to BatteryVoltageConverter - Rename method battStr2mv to batteryStringToVoltage - Rename mthod battmv2Str to batteryVoltageToString

---
+++ app/src/main/java/vn/com/tma/idlesmart/BatteryVoltageConverter.java
@@ -0,0 +1,30 @@
+package vn.com.tma.idlesmart;
+
+
+public class BatteryVoltageConverter {
+
+    /**
+     * Convert string to voltage
+     * @param batteryString
+     * @return the number of voltage
+     */
+    public int batteryStringToVoltage(String batteryString) {
+        try {
+            return (Integer.parseInt(batteryString.substring(0, 2)) * 10) + Integer.parseInt(batteryString.substring(3, 4));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    /**
+     * Convert voltage to string
+     * @param batteryVoltage
+     * @return volt string
+     */
+    public String batteryVoltageToString(int batteryVoltage) {
+        int volts = batteryVoltage / 10;
+        String voltstr = Integer.toString(volts);
+        return voltstr + "." + Integer.toString(batteryVoltage - (volts * 10));
+    }
+}
