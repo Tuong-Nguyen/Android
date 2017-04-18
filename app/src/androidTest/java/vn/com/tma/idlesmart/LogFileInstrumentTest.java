bc2cfdc0337591eba3c7e2cfd356fe0ea6c4d8fe
Hanh <ntmhanh@tma.com.vn>
2017-04-18 18:03:40 +0700

    - Instrument test for read and write

--- app/src/androidTest/java/vn/com/tma/idlesmart/LogFileInstrumentTest.java
+++ app/src/androidTest/java/vn/com/tma/idlesmart/LogFileInstrumentTest.java
@@ -12,10 +12,12 @@ import org.junit.runner.RunWith;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import vn.com.tma.idlesmart.Utils.LogFile;
 
 import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by ntmhanh on 4/14/2017.
@@ -59,5 +61,25 @@ public class LogFileInstrumentTest {
             // Assert
             assertEquals("File was deleted ", true, isDelete);
         }
+    @Test
+    public void readwrite_inputStringData_returnDataExistInThatFile() throws IOException {
+        //Arrange
+        String fileNamePath = "Logs";
+        String filename = "Log1.bin";
+        String tag = "test";
+        context = InstrumentationRegistry.getTargetContext();
+        LogFile logFile = new LogFile(context, filename, fileNamePath, tag);
+
+        // Action
+        logFile.deleteFile(filename);
+        logFile.write("This is instrument test!");
+        String data = "";
+        data = logFile.readString();
+
+        // Assert
+        assertNotEquals("", data);
+
+
+    }
 
 }
