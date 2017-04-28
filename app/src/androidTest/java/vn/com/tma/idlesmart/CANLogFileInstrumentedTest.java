7b810ad66847a9b54d3fbc6193e95b0d189737ce
Hanh <ntmhanh@tma.com.vn>
2017-04-28 14:45:38 +0700

    - Add setup before run test for CANLogFileInstrumentTest

--- app/src/androidTest/java/vn/com/tma/idlesmart/CANLogFileInstrumentedTest.java
+++ app/src/androidTest/java/vn/com/tma/idlesmart/CANLogFileInstrumentedTest.java
@@ -3,6 +3,7 @@ package vn.com.tma.idlesmart;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -14,22 +15,28 @@ import static junit.framework.Assert.assertTrue;
 
 public class CANLogFileInstrumentedTest {
     private Context context;
+    private CANLogFile canLogFile;
+    private String fileNamePath = "CANLogs";
+    private String filename = "CANLog.bin";
+    private String tag = "Test";
+    @Before
+    public void setUp() {
+        //Arrange
+        context = InstrumentationRegistry.getTargetContext();
+        canLogFile = new CANLogFile(context, filename, fileNamePath, tag);
+    }
+
 
     @Test
     public void writeCANLogFile_inputStringDataIntoANewFile_returnTheDataExistInThatFile() throws IOException {
         //Arrange
-        String fileNamePath = "CANLogs";
-        String filename = "CANLog.bin";
-        String tag = "test";
-        context = InstrumentationRegistry.getTargetContext();
-        CANLogFile canLogFile = new CANLogFile(context, filename, fileNamePath, tag);
         canLogFile.deleteFile(filename);// Ensure this is new file
         String input = "This is instrument test";
 
         // Action
         canLogFile.write(input);
         String data = canLogFile.read();
-        boolean result = data.matches("(?i).*This is instrume.*");
+        boolean result = data.matches(".*\\sThis is the first input\\n");
 
         // Assert
         assertTrue(result);
@@ -38,28 +45,16 @@ public class CANLogFileInstrumentedTest {
     @Test
     public void writeCANLogFile_inputStringDataIntoExistFile_returnOldAndNewDataInThatFile() throws IOException {
         //Arrange
-        String fileNamePath = "CANLogs";
-        String filename = "CANLog2.bin";
-        String tag = "test";
         String input1 = "This is the first input";
         String input2 = "This is the second input";
-        context = InstrumentationRegistry.getTargetContext();
-
-        CANLogFile canLogFile = new CANLogFile(context, filename, fileNamePath, tag);
         canLogFile.deleteFile(filename);// Ensure this is a new file
 
         // Action
         canLogFile.write(input1);
         canLogFile.write(input2);
         String data = canLogFile.read();
-        boolean result1 = data.matches("(?i).*This is the firs.*");
-        boolean result2 = data.matches("(?i).*This is the seco.*");
-        boolean result;
-        if (result1 == true && result2 == true){
-            result = true;
-        } else {
-            result = false;
-        }
+        boolean result = data.matches(".*\\sThis is the first input\\n.*\\sThis is the second input\\n");
+
         // Assert
         assertTrue(result);
     }
