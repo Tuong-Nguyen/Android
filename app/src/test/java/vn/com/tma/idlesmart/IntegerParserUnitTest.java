f7127306bf85015a1cd378c517ed5f5ea83ae8da
Hanh <ntmhanh@tma.com.vn>
2017-05-08 17:03:53 +0700

    - Modify IntegerParser test case

--- app/src/test/java/vn/com/tma/idlesmart/IntegerParserUnitTest.java
+++ app/src/test/java/vn/com/tma/idlesmart/IntegerParserUnitTest.java
@@ -22,14 +22,14 @@ public class IntegerParserUnitTest {
         assertEquals(20, num);
     }
     @Test
-    public void toInteger_InputStringIsNotNumber_ReturnIntegerOfThisNumber(){
+    public void toInteger_InputStringIsNotNumber_Return0(){
         //Arrange
         IntegerParser integerParser = new IntegerParser();
-        String textNum = "20";
+        String textNum = "A20";
         //Action
         int num = integerParser.toInteger(textNum);
         //Assert
-        assertEquals(20, num);
+        assertEquals(0, num);
     }
 
 }
