f5ea7217281a7f13ebb854c0970918a64b7a8453
Hanh <ntmhanh@tma.com.vn>
2017-05-08 15:47:43 +0700

    - Move Time2MinsSecsStr from MainActivity to TimeConverter

---
+++ app/src/main/java/vn/com/tma/idlesmart/Utils/TimeConverter.java
@@ -0,0 +1,17 @@
+package vn.com.tma.idlesmart.Utils;
+
+/**
+ * Created by ntmhanh on 5/8/2017.
+ */
+
+public class TimeConverter {
+
+    public String Time2MinsSecsStr(int time) {
+        int mins = time / 60;
+        int secs = time - (mins * 60);
+        if (secs < 10) {
+            return Integer.toString(mins) + ":0" + Integer.toString(secs);
+        }
+        return Integer.toString(mins) + ":" + Integer.toString(secs);
+    }
+}
